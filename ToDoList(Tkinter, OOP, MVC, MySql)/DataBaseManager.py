c12a7d4f5fe59daf551a16b7312243084e88f436
Kubek1984 <kubek@polandmail.com>
2023-06-29 21:29:28 +0100

    refactor

--- ToDoList(Tkinter, OOP, MVC, MySql)/DataBaseManager.py
+++ ToDoList(Tkinter, OOP, MVC, MySql)/DataBaseManager.py
@@ -26,25 +26,3 @@ class DataBaseManager:
     def commit(self):
         self.connection.commit()
 
-# name = 'aaaaaa'
-# text = 'aaaaaa'
-# priority = 'aaaaa'
-# date_now = datetime.datetime.now().date()
-# # print(type(date_now))
-# date_now_string = date_now.strftime('%Y-%m-%d')
-# print(type(date_now_string))
-x=DataBaseManager()
-# x.execute(f'INSERT INTO task(name, text, priority, date) VALUES({name}, {text}, {priority}, {date_now_string});')
-
-# query = f"INSERT INTO 'task' ('id', 'name', 'text', 'priority') VALUES (NULL, {name}, {text}, {priority});"
-# x.execute(f"INSERT INTO `task` (`id`, `name`, `text`, `priority`, `date`) VALUES (NULL, '{name}', '{text}', '{priority}', '{date_now_string}');")
-
-# x.execute(f"INSERT INTO `task` (`id`, `name`, `text`, `priority`) VALUES (NULL, 'sdsdf', 'dfsdf', 'dsfsdfsd');")
-# "INSERT INTO `task` (`id`, `name`, `text`, `priority`, `date`) VALUES (NULL, 'Testing task', 'random text', 'High', '2023-06-27');"
-# x.commit()
-# result = x.execute("SELECT * FROM task;")
-# print(result)
-# x.execute('DELETE FROM `task` WHERE `task`.`id` = 3 ')
-# x.commit()
-result1 = x.execute("SELECT * FROM task;")
-print(result1)
